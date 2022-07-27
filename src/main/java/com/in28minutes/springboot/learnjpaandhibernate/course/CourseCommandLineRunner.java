606b974ef9b13c148562780d9520f08b5181c8f1
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-07-27 19:29:32 +0530

    Getting Started with Spring Data JPA

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -6,26 +6,35 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
+	
 	
 	@Override
 	public void run(String... args) throws Exception{
-		repository.insert(new Course(1,"Learn JPA Now!","Rajeev"));
-		repository.insert(new Course(2,"Learn Azure!","Rajeev"));
-		repository.insert(new Course(3,"Learn Java!","Rajeev"));
-		repository.insert(new Course(4,"Learn Angular!","Rajeev"));
-		repository.insert(new Course(5,"Learn Full Stack!","Rajeev"));
-		repository.insert(new Course(6,"Learn ML!","Rajeev"));
+		repository.save(new Course(1,"Learn JPA Now!","Rajeev"));
+		repository.save(new Course(2,"Learn Azure!","Rajeev"));
+		repository.save(new Course(3,"Learn Java!","Rajeev"));
+		repository.save(new Course(4,"Learn Angular!","Rajeev"));
+		repository.save(new Course(5,"Learn Full Stack!","Rajeev"));
+		repository.save(new Course(6,"Learn ML!","Rajeev"));
 		
-		repository.deleteById(2);
+		repository.deleteById(2l);
 		
-		System.out.println(repository.findById(3));
-		System.out.println(repository.findById(5));
+		System.out.println(repository.findById(3l));
+		System.out.println(repository.findById(5l)); //adding l, because method expects long
+		
 	}
 	
 	
